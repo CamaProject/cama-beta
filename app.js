0d6f5ab77cd3a156d23600af6031b8c957e82eff
Patrick_Inshuti <pim219@nyu.edu>
2018-04-17 22:23:54 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+//*** Routes ***//
+
+// Going to Index Page (Landing page)
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+// Going to Resouces
+app.get("/resources", function(req, res){
+  res.render("resources");
+});
+
+
+//Going to Executive Members
+app.get("/executives", function(req, res){
+  res.render("executive-team");
+});
+
+//Going to Mentors
+app.get("/mentors", function(req, res){
+  res.render("mentors");
+});
+
+//Going To Join
+app.get("/join", function(req, res){
+  res.render("join");
+});
+
+//*** End of Routes ***//
+
+//Deploying Server
+var port = 3000;
+app.listen(port||process.env.PORT, function(req, res){
+  console.log("Server Up and Running at"+" "+port);
+})
