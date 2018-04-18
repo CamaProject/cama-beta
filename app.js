2980963ef6938cc26d0a6aa68fc5da851e2ab473
Patrick_Inshuti <pim219@nyu.edu>
2018-04-18 22:07:18 +0400

    Made Alot of changes

--- app.js
+++ app.js
@@ -5,11 +5,67 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
+//******** Adding Mentors Data ********//
+
+
+var mentorList = [
+{ name:"Sharon", country:"Canada", bio:"Sharon is a biology major specializing in the neurosciences at New York University Abu Dhabi. She was born in China but has called Toronto, Canada home for most of her life. Besides the neurosciences, she also has an interest in genetics, and for her senior thesis, she is researching genes conferring drug resistance in the malaria parasite in Burkina Faso. She also enjoys reading and learning languages, and has thus completed minors in literature and in French studies. At NYUAD, she serves as a peer tutor in the Writing Centre, an intern in the Dean of Students’ Office, and as a leader for several student groups including Abu Dhabi Christian Fellowship, Attitude Dance Society, and Women Empowered in STEM. She loves working with others and building community through volunteering, organizing events, and establishing friendships across cultural and linguistic boundaries. She looks forward to serving as a mentor with CAMA", image:"img/mentors/sharon.PNG"},
+
+{name:"Maya", country:"Uganda", bio:"Maya is a Visual Art's Practice Major with a concentration in Social Research and Public Policy and the Environment. She is particularly interested in social, economic, and cultural environmental sustainability.", image:"img/mentors/maya.PNG"},
+
+{name:"Luis", country:"Mexico", bio:"Luis Francisco studies Political Science and Arabic at New York University Abu Dhabi. He is very interested in learning languages, and talking with people about their culture and background. Even though he was born and raised in Mexico, he is interested in Arab history, politics, and language. Still, he enjoys talking about all kinds of interesting things. After graduating, Francisco plans to work in diplomacy in the region, but he is yet to see what will happen in the next years before he finishes college. He loves listening to music, playing tennis, watching a good movie, or reading interesting books.", image:"img/mentors/luis.png"},
+
+{name:"Santina", country:"United States", bio:"Santina is a second-year student at New York University Abu Dhabi from the United States, majoring in Social Research and Public Policy. Her academic interests include history and psychology and learning how an understanding of contingent circumstances coupled with an understanding of characteristics of human behavior provide insight into how one can create effective public policies to address today's pressing issues. Santina also loves spending time outdoors, and with family and friends!", image:"img/mentors/santina.PNG"},
+
+{name:"Shaqe", country:"Armenia", bio:"Shaqe Karapetyan is a rising sophomore from Armenia at NYUAD. She studies Physics and minors in Music and Computer Science. Her main interests are in theoretical physics. Shaqe is very much into music and performance as well as social service and volunteering. Prior to university she completed the International Bachelor program and applied to UK universities too, so she is familiar with the UCAS system as well as the Common App. Shaqe is approachable and always happy to answer any questions.", image:"img/mentors/shaqe.PNG"},
+
+
+{name:"Mahrukh", country:"Pakistan", bio:"Mahrukh Tauseef is a Pakistani who is a rising sophomore at New York University Abu Dhabi. She is a practicing Muslim. She aims to pursue a career in Electrical Engineering. Her primary field of interest is Bio-robotics and she dreams to work on this interesting merge of biology and engineering to provide advancement in the field of medicine. Apart from academics, she loves to swim, read, write poems, listen to music, watch sitcoms, and participate in any social work opportunities. She is a member of many student interest groups on campus. She loves to travel and explore new cultures. She is friendly and funny and she loves to get to know people.", image:"img/mentors/m.PNG"},
+
+{name:"Juria", country:"Japan", bio:"Juria, a sophomore at NYU Abu Dhabi, calls Japan and Nepal her home. While she spent her childhood growing up on the seashore of Chiba, she did most of her schooling in the city of Kathmandu. At NYU Abu Dhabi, she is currently studying Economics and enjoys learning topics in public policy and development. She is also an environment enthusiast and hopes to minor in Environmental Studies. Beyond academics, she is passionate about, and committed to serving the community. At NYU Abu Dhabi, she conducts various recreational and educational activities for domestic migrant workers in Abu Dhabi. In her free time, she enjoys being outdoors and always seeks to try out new adventures; traveling and exploring new cultures has been a favorite part of it. She has always had an interest in trekking in the mountains of Nepal, biking through rocky trails, climbing steep rocks, and trying out new cuisines.", image:"img/mentors/juria.PNG"},
+
+{name:"Dhruvi", country:"Kenya", bio:"Dhruvi is a rising sophomore on the path to becoming a Civil Engineer at New York University Abu Dhabi. Her interest in the field stemmed from a project she was involved in while completing the IB Diploma in high school. The project focused on raising funds to build a sand dam in a rural part of her home town, Kenya.She realized her love for Science and Mathematics when she designed a learning platform to motivate young girls to study those subjects. Encouraging the youth, and especially girls to pursue Science, Technology, Engineering and Mathematics subjects has been an area of interest for her. While studying at NYUAD she was part of the weSTEM High School Girls Conference aimed at empowering young girls to take up such fields at a higher level. She believes that education is an essential tool which provides individuals with the skills to achieve their dreams.", image:"img/mentors/dhruvi.PNG"},
+
+
+{name:"Alison", country:"United States", bio:"Alison is an electrical engineering student at NYU Abu Dhabi from California, U.S.A. She is interested in biomedical technology and robotics. In high school, she taught piano lessons to beginning students and led an Academic Decathlon competition time. Since coming to NYUAD, she has been involved in a variety of activities including vocal ensemble, volunteering at the MBZ Women's Labor Camp, and Interfaith Dialogue. Her time working as an intern for the NYU Abu Dhabi Admissions Office developed her interest in college advising and education, as she communicated regularly with prospective students and high school counselors. In her free time, she enjoys playing the piano, reading, traveling, and hiking.", image:"img/mentors/alison.PNG"},
+
+{name:"JooHee", country:"South Korea", bio:"JooHee is a fourth year student at NYU Abu Dhabi. She is majoring in Biology, with a special interest in Genetics and Bioinformatics. She hopes to eventually conduct population genetics research with clinical applications to public health, as she believes a healthy body and mind are quintessential to most forms of success. Besides being a mentor at CAMA, JooHee is also a core team member of the Public Health Think Tank, an annual conference that brings together youth to develop interventions for pressing health issues in the UAE. Born in South Korea, she has also spent parts of her childhood growing up in Atlanta, Georgia. In her free time she enjoys swimming, dancing, and writing film critiques. Through CAMA, she looks forward to making meaningful connections with young individuals and providing help in the ways she can.", image:"img/mentors/joohee.png"},
+
+{name:"Anita", country:"Czech Republic", bio:"Anita Duskova comes from the Czech Republic where she studied at the Johannes Kepler Grammar School in Prague. Already there she expressed her interest in debate by participating in MUN and Model European Parliament. She also took part in golf, swimming and equestrian. She continued many of these activites at NYUAD, where she became the president of the Equestrian SIG and represented the school in FinMUN. Anita's passion for debate showed in her major - Political Science and Philosophy, where she is specifically interested in Political Theorists such as Tocqueville, Weber and Friedman. Anita's further interest lies in educational policies, which is why she joined CAMA. She wants to learn about different conditions people have from different countries, in hope of finding means of equalizing conditions in access to education worldwide.", image:"img/mentors/anita.PNG"},
+
+{name:"AhRam", country:"South Korea", bio:"AhRam is a rising sophomore at New York University Abu Dhabi. She is from South Korea, and graduated from CheongShim International Academy in the same country. AhRam is deciding between mathematics and economics for her major choice at university. She joined CAMA because she hoped that college application process could be a little less overwhelming and possibly enjoyable with a student mentor to talk freely to. AhRam is interested in languages, with a certain level of proficiency in several. She reads about linguistics and has won an award in the International Linguistics Olympiad. She also enjoys creative writing, grammar, vocabularies and worked as an essay tutor. Her college essays revolve around her linguistic interests and musical theater experience during high school.", image:"img/mentors/a.PNG"},
+
+
+{name:"Denat", country:"Ethiopia", bio:"Denat E. Negatu is a rising sophomore at New York University Abu Dhabi. Although she is undecided about her major, she is interested in science, math, and economics. Experiencing different educational systems in her academic journey showed her the need and importance of the quality education for young students. Her desire to make quality education available for young students developed through teaching at humanitarian organizations and tutoring disadvantaged students in her home city Addis Ababa. In her free time, Denat enjoys dancing, playing soccer and, most importantly she loves spending time with her dogs! As a mentor in the CAMA program, she hopes to make the process of applying to universities easier for prospective freshmen and bright students.", image:"img/mentors/denat.PNG"},
+
+{name:"Joel", country:"Rwanda", bio:"Joel is a civil engineering senior from Rwanda. Joel enjoys photography and socio-economic politics and development.", image:"img/mentors/joel.PNG"},
+
+{name:"Ikenna", country:"Nigeria", bio:"Ikenna is a rising Junior at New York University Abu Dhabi currently majoring in Computer Engineering with a minor in Computer Science. He is interested in Artificial Intelligence/Deep Learning and being happy. Ikenna has always been passionate about knowing/learning how things work and strongly believes that education is an investment that yields the best interest. He looks forward to working with African students interested in pursuing educational opportunities outside the boundaries of their country.", image:"img/mentors/ikenna.PNG"},
+
+{name:"Liam", country:"United States", bio:"Liam is a third-year student hailing from St. Louis, Missouri in the United States. At NYUAD he is majoring in Physics, but also is taking in as many philosophy courses as he can, his other passion. Committed to furthering knowledge, he has contributed to research in astrophysics, artificial intelligence, and climate change while at NYUAD. He plans on continuing to graduate school in one of these fields. When not busy at work, you can find him seeking out the best food in Abu Dhabi, on the court playing for NYUAD basketball team, or rock-climbing in the mountains.", image:"img/mentors/liam.PNG"},
+
+{name:"Rogers", country:"Zambia", bio:"Rogers Iradukunda is a rising senior at New York University Abu Dhabi who is majoring in Civil Engineering. Upon completing high school, Rogers participated in the United States Students Achievers Program (USAP), a program that assists high school students from select countries to apply to US institutions of higher learning. Rogers is therefore knowledgeable about the application process associated with US colleges and universities, and would love to assist prospective freshmen embarking on a similar path to curate a worthwhile application. In his free time, Rogers loves to watch movies, listen to music, play soccer and make conversation with friends.", image:"img/mentors/rogers.PNG"},
+
+{name:"Chiran", country:"Nepal", bio:"Chiran is a student from Nepal majoring in Literature and Philosophy at NYU Abu Dhabi. In his free time, he reads Gabriel Garcia Marquez, listens to rap music, and tries to write poetry. At NYUAD, he works together with young high school students in various social organizations. He wants to be a scholar and an educator in the future.", image:"img/mentors/chiran.PNG"},
+
+{name:"Haruna", country:"Ghana", bio:"Haruna goes by the name Hajj Haroun, a native of Ghana – a country found in the western part of Africa and popularly known for its peaceful and hospitable citizens. Growing up in the cultures of Ghana, he has learnt the timeless virtue of living not only for oneself but for the larger society. Haruna is currently a third year student in the Humanities at NYUAD, and plans to join the workforce immediately after graduation. Haruna believes that no one is perfect. We can be great at everything but there is always someone out there that will be willing to help us work on our weaknesses. Hajj is willing to be that one person.", image:"img/mentors/haruna.PNG"},
+
+{name:"Jessica", country:"Nigeria", bio:"Jessica is a junior majoring in Social Research and Public Policy with a minor in Economics. She developed a passion for helping young people achieve their dreams when she worked with students at Brainfield Schools, Nigeria. As a freshman at NYUAD, she was a co-facilitator of Girls' Education Network. Jessica is grateful for an opportunity of studying abroad and wants to give back by guiding high school students. She is currently a Resident Assistant (RA) at NYUAD and has also gained experience by working with the NYUAD office of Public Affairs and NYUAD Career Development Center. Jessica is keen on becoming a consultant and has stepped onto her career path with internships at Select Training and Management Consultancy (UAE) as well as South African Renewable Energy Business Incubator (South Africa).", image:"img/mentors/jessica.PNG"}
+
+
+
+];
+
+
+
+
+//******** Adding Mentors Data ********//
+
 //*** Routes ***//
 
 // Going to Index Page (Landing page)
 app.get("/", function(req, res){
-  res.render("index");
+  res.render("index",{mentorList:mentorList});
 });
 
 // Going to Resouces
@@ -22,18 +78,55 @@ app.get("/resources", function(req, res){
 app.get("/executives", function(req, res){
   res.render("executive-team");
 });
+app.get("/executives/", function(req, res){
+  res.render("eboardexecutive-team");
+});
+app.get("/executives", function(req, res){
+  res.render("advisoryexecutive-team");
+});
+
+
 
 //Going to Mentors
-/*app.get("/mentors", function(req, res){
-  res.render("mentors");
-}); */
+app.get("/mentors", function(req, res){
+  res.render("mentors", {mentorList:mentorList});
+});
+
+app.get("/mentors/current", function(){
+  res.render("currentMentors");
+});
+
+app.get("/mentors/previous", function(){
+  res.render("previousMentors");
+});
+
+//End of Mentors //
+
 
 //Going To Join
 app.get("/join", function(req, res){
   res.render("join");
 });
 
+
+//Going to MENTEES
+app.get("/mentees", function(req, res){
+  res.render("mentees");
+});
+
+app.get("/mentees/current", function(){
+  res.render("currentMentees");
+});
+
+app.get("/mentees/previous", function(){
+  res.render("previousMentees");
+});
+
+//End of Mentors //
+
+
 //*** End of Routes ***//
+
 
 //Deploying Server
 
